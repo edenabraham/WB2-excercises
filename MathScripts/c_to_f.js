009200a16f47cfcfed4e7d7d09838db72ee4af82
Eden Abraham <eabraham@atl.yearup.org>
2022-09-27 18:48:19 -0400

    celsius converted to fahrenheit

---
+++ MathScripts/c_to_f.js
@@ -0,0 +1,5 @@
+//celsius to fahrenheit
+//F = C x (9/5) + 32
+var celsius= 24
+var fahrenheit= celsius*(9/5)+32;
+console.log("If it is " +celsius+ " degrees celsius, it is "+fahrenheit+ " degrees fahrenheit");
