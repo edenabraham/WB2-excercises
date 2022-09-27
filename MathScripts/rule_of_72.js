8ff22f198f4bee92f02c61e28bf8df793f4a40b2
Eden Abraham <eabraham@atl.yearup.org>
2022-09-27 18:17:50 -0400

    rule of 72

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,5 @@
+// rule of 72
+var savingsAccountWorth=2000
+var interestRate=4
+console.log("At a "+interestRate+"% interest rate, your savings account will be worth $"+
+savingsAccountWorth*2+" in "+72/interestRate+" years");[]
