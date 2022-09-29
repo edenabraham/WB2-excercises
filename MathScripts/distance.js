ca8244705cd38219988fbbbff0d9bea6e91de6b4
Eden Abraham <eabraham@atl.yearup.org>
2022-09-28 21:43:39 -0400

    distance

---
+++ MathScripts/distance.js
@@ -0,0 +1,9 @@
+//formula for distance btween (x1,y1) and (x2,y2)
+// distance= sqrt of (x2-x1)^2+(y2-y1)^2
+var x1=2
+var x2=4
+var y1=3
+var y2=5
+var distance= Math.sqrt((x2-x1)^2+(y2-y1)^2)
+console.log("The distance between ("+x1+","+y1+") and ("+x2+","+y2+") is "+distance);
+
